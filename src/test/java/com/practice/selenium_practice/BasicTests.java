f75bc81ef34ede3ea1e9c187b40f6cca1f53e2ef
manngndu <manpreesingh@bebotechnologies.com>
2020-11-27 16:17:08 +0530

    simple paralled tests added

--- src/test/java/com/practice/selenium_practice/BasicTests.java
+++ src/test/java/com/practice/selenium_practice/BasicTests.java
@@ -1,10 +1,19 @@
 package com.practice.selenium_practice;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,26 +23,36 @@ public class BasicTests {
 	WebDriver driver;
 	
 	@BeforeMethod
-	public void setup() {
-		System.setProperty("webdriver.chrome.driver", "D:\\practice\\utilities\\chromedriver.exe");		
-		driver = new ChromeDriver();
+	public void setup() throws MalformedURLException {		
+		DesiredCapabilities	desiredCapabilities = new DesiredCapabilities();
+		desiredCapabilities.setBrowserName("chrome");		
+		desiredCapabilities.setPlatform(Platform.WIN10);
+		
+		ChromeOptions options = new ChromeOptions();
+		options.merge(options);		
+		
+		String hubUrl = "http://192.168.225.169:4444/wd/hub";		
+		driver = new RemoteWebDriver(new URL(hubUrl),options);		
+		
+	}
+
+	@Test
+	public void titleTest() {	
+		System.out.println("Title Test is executed by Thread =>"+Thread.currentThread().getName()+"-----------"+Thread.currentThread().getId());
 		driver.get("https://www.google.com");
+		Assert.assertEquals("Google", driver.getTitle());		
 	}
 
 	@Test
-	public void titleTest() {		
-		Assert.assertEquals("Google", driver.getTitle());
-	}
-
-	@Test
-	public void searchBarTest() {		
+	public void searchBarTest() {	
+		driver.get("https://www.google.com");
+		System.out.println("SearchBar Test is executed by Thread =>"+Thread.currentThread().getName()+"-----------"+Thread.currentThread().getId());
 		WebElement searchBar = driver.findElement(By.name("q"));
-		Assert.assertTrue(searchBar.isDisplayed());
+		Assert.assertTrue(searchBar.isDisplayed());		
 	}
 	
-	@AfterTest
+	@AfterMethod
 	public void teardown() {
 		driver.quit();
 	}
-
 }
