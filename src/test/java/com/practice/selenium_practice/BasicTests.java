6c5a3e77cc979bdaea852965f66fee3e8082d37c
manngndu <manpreesingh@ARVON-QA1.bebotechnologies.com>
2020-11-27 13:48:23 +0530

    added simple tests

--- src/test/java/com/practice/selenium_practice/BasicTests.java
+++ src/test/java/com/practice/selenium_practice/BasicTests.java
@@ -1,7 +1,10 @@
 package com.practice.selenium_practice;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -18,10 +21,16 @@ public class BasicTests {
 	}
 
 	@Test
-	public void titleTest() {
-		System.out.println("The title of the page is"+driver.getTitle());
+	public void titleTest() {		
+		Assert.assertEquals("Google", driver.getTitle());
 	}
 
+	@Test
+	public void searchBarTest() {		
+		WebElement searchBar = driver.findElement(By.name("q"));
+		Assert.assertTrue(searchBar.isDisplayed());
+	}
+	
 	@AfterTest
 	public void teardown() {
 		driver.quit();
